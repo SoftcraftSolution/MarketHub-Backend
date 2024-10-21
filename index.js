4d246de53cdf985ae67a457e2f3f485bb6ab22ee
SoftcraftSolution <168642737+SoftcraftSolution@users.noreply.github.com>
2024-10-21 17:57:55 +0530

    djbjb

--- index.js
+++ index.js
@@ -12,19 +12,13 @@ const http = require('http');
 const app = express();
 const port = process.env.PORT || 3000; // Use port from environment variable or default to 3000
 
-// Log the MongoDB URI to check if it's defined
-
-
 // Middleware
 app.use(cors());
 app.use(bodyParser.json({ limit: '100mb' })); // Adjust the limit as needed
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB Connection
-mongoose.connect("mongodb+srv://Rahul:myuser@rahul.fack9.mongodb.net/MarketHub?authSource=admin&replicaSet=atlas-117kuv-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect("mongodb+srv://Rahul:myuser@rahul.fack9.mongodb.net/MarketHub?authSource=admin&replicaSet=atlas-117kuv-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true")
 .then(() => {
   console.log('Connected to MongoDB');
 })
